bb4390d4af61aeba5c96a41c961908d311518a28
CocoaBob <mengke.wang@gmail.com>
2022-06-22 16:30:25 -0400

    I don't know which version is newer, at least this version works.

--- README.md
+++
@@ -1,1 +0,0 @@
-**Not Maintained because node.js can't really run on very low power devices, see : ** [https://github.com/sebpiq/FreedomPortal](https://github.com/sebpiq/FreedomPortal)
--- examples/openAllInCna.js
+++ examples/openAllInCna.js
@@ -3,8 +3,9 @@ var fs = require('fs')
 var path = require('path')
 var express = require('express')
 var CaptivePortal = require('../index').CaptivePortal
-var IosCnaHandler = require('../index').IosCnaHandler
-var AndroidCnaHandler = require('../index').AndroidCnaHandler
+var CnaIosClient = require('../index').CnaIosClient
+var CnaAndroidClient = require('../index').CnaAndroidClient
+CaptivePortal.clientClasses = [CnaAndroidClient, CnaIosClient]
 
 // Start an HTTPS server with self-signed certificates
 // This server simply redirects to our http server
@@ -20,22 +21,11 @@ httpsServer.listen(443, function() { console.log('https server listening') })
 
 // HTTP server with captive portal
 var httpApp = express()
-var captivePortal = new CaptivePortal([ 
-  new IosCnaHandler(), new AndroidCnaHandler()
-])
-captivePortal.on('error', function(err) {
-  console.error(err)
-})
-
+var captivePortal = new CaptivePortal()
 httpApp.use(captivePortal.handler)
 httpApp.get('/', function(req, res) { res.sendFile('home.html', { root: __dirname }) })
 httpApp.get('/a', function(req, res) { res.sendFile('a.html', { root: __dirname }) })
 httpApp.get('/b', function(req, res) { res.sendFile('b.html', { root: __dirname }) })
 httpApp.use('/assets', express.static(__dirname + '/assets'))
 httpApp.get('*', function(req, res) { res.redirect('/') })
-httpApp.listen(80, function() { 
-  captivePortal.start(function(err) {
-    if (err) throw err
-    console.log('http server listening') 
-  })
-})
+httpApp.listen(80, function() { console.log('http server listening') })
